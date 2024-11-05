1537c1b47c180ff419d0612c18598a54241a9940
coursar <coursar@gmail.com>
2024-11-05 13:46:07 +0300

    function call from html

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 'use strict';
 
 // function:
-// 1. function <name>(<param>) { <body> }
-{
-    function calculateCashback() {
-        const amount = 10_000;
-        const percent = 0.006;
-        const additionalTax = 20;
-        const tax = amount * percent + additionalTax; // VS Code -> Ctrl + Shift + R -> Refactoring
-        console.log(tax);
-    }
+// 1. function <name>(<param>) { <body> } -> "всплывает" (но не так, как var)
+// 2. const <name> = function(<param>) { <body> };
+function calculateCashback() {
+    const amount = 10_000;
+    const percent = 0.006;
+    const additionalTax = 20;
+    const tax = amount * percent + additionalTax; // VS Code -> Ctrl + Shift + R -> Refactoring
+    console.log(tax);
 }
