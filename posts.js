efe5e146de9a8147acba670a0b161e7a3d2476be
coursar <coursar@gmail.com>
2024-11-08 12:16:49 +0300

    posts data

---
+++ posts.js
@@ -0,0 +1,31 @@
+// webpack, ro*
+(() => {
+    // TODO:
+    // 0. from server data
+    // 1. ref to root element (container) div[data-id=posts]
+    // 2. create children inside container
+    // 2.1. create element
+    // 2.2. container.appendChild, container.append
+
+    const posts = [
+        {
+            id: 1,
+            name: 'Преступление и наказание',
+            image: 'https://placehold.co/200x100',
+            likes: 0,
+        },
+        {
+            id: 2,
+            name: 'Татьяна',
+            image: 'https://placehold.co/200x100',
+            likes: 0,
+        },
+        {
+            id: 3,
+            name: 'Элементарно',
+            image: 'https://placehold.co/200x100',
+            likes: 2,
+        },
+    ]
+
+})();
